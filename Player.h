f486d896f8e82528d889aa117ff10d79e9efa797
Javier Sosa <javiergsosaromero05@gmail.com>
2025-03-27 00:08:25 -0400

    bug found fix it

--- Player.h
+++ Player.h
@@ -1,35 +1,41 @@
 #pragma once
+#include <string>
 #include"Item.h"
-#include<string>
-class Item; 
+
+// Forward declaration of the Item class
+class Item;
 
 class Player {
 public:
-	//wanna make a constructor set the values instead of set and get for now, Iwill do that later 
-	/*Player() : health(0), name("NULL"), attack(0), max(10), size(0) {
-		inventory = new Item[max];
+	Player() : health(0), name("NULL"), attack(0), max(10), size(0) {
+		//inventory = new Item[max];  // Allocate the array of Item objects
+		for (int i = 0; i < max; i++) {
+			//inventory[i] = Item("Nothing", 0);  // Set custom values for each Item
+		}
 	};
-	*/
 	Player(std::string inname, int inhealth, int inattack) :
 		health(inhealth), name(inname), attack(inattack), max(10), size(0) {
-		inventory = new Item[max];
-		inventory = nullptr;
+		//inventory = new Item[max];
+		//Item* nothing = new Item("Nothing", 0);
+		for (int i = 0; i < max; i++) {
+			//inventory[i] = Item("Nothing", 0);
+		}
 	};
 	~Player() {
-		delete []inventory;
+		delete[]inventory;
 	};
-	void Display(int number);
-	bool AddItem(Item* inItem);
-	//or int IndexItem( std::string itemname);
-	int IndexItem( std::string itemname);
-	void Usescroll(Player* playername,Item* scroll);
-	void DisplayInv();
-	
+
+    void Display(int number);
+    bool AddItem(Item* inItem);
+    int IndexItem(std::string itemname);
+    void Usescroll(Player* playername, Item* scroll);
+    void DisplayInv();
+
 private:
-	int health;
-	std::string name;
-	int attack;
-	int max; 
-	int size;
-	Item* inventory;
+    int health;
+    std::string name;
+    int attack;
+    int max;
+    int size;
+    Item* inventory; // Just a pointer here, no need to include Item.h
 };
