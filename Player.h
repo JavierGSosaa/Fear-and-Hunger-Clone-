0d385e7d0e06b659eb17025f88eda14b408c9fa7
Javier Sosa <javiergsosaromero05@gmail.com>
2025-03-27 00:09:42 -0400

    Revert "bug found fix it"
    This reverts commit f486d896f8e82528d889aa117ff10d79e9efa797.

--- Player.h
+++ Player.h
@@ -1,41 +1,35 @@
 #pragma once
-#include <string>
 #include"Item.h"
-
-// Forward declaration of the Item class
-class Item;
+#include<string>
+class Item; 
 
 class Player {
 public:
-	Player() : health(0), name("NULL"), attack(0), max(10), size(0) {
-		//inventory = new Item[max];  // Allocate the array of Item objects
-		for (int i = 0; i < max; i++) {
-			//inventory[i] = Item("Nothing", 0);  // Set custom values for each Item
-		}
+	//wanna make a constructor set the values instead of set and get for now, Iwill do that later 
+	/*Player() : health(0), name("NULL"), attack(0), max(10), size(0) {
+		inventory = new Item[max];
 	};
+	*/
 	Player(std::string inname, int inhealth, int inattack) :
 		health(inhealth), name(inname), attack(inattack), max(10), size(0) {
-		//inventory = new Item[max];
-		//Item* nothing = new Item("Nothing", 0);
-		for (int i = 0; i < max; i++) {
-			//inventory[i] = Item("Nothing", 0);
-		}
+		inventory = new Item[max];
+		inventory = nullptr;
 	};
 	~Player() {
-		delete[]inventory;
+		delete []inventory;
 	};
-
-    void Display(int number);
-    bool AddItem(Item* inItem);
-    int IndexItem(std::string itemname);
-    void Usescroll(Player* playername, Item* scroll);
-    void DisplayInv();
-
+	void Display(int number);
+	bool AddItem(Item* inItem);
+	//or int IndexItem( std::string itemname);
+	int IndexItem( std::string itemname);
+	void Usescroll(Player* playername,Item* scroll);
+	void DisplayInv();
+	
 private:
-    int health;
-    std::string name;
-    int attack;
-    int max;
-    int size;
-    Item* inventory; // Just a pointer here, no need to include Item.h
+	int health;
+	std::string name;
+	int attack;
+	int max; 
+	int size;
+	Item* inventory;
 };
